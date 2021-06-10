224a82e63b86204d0567d759eb81fbec86af436f
유상욱 <tkddnr032@naver.com>
2021-06-10 18:42:09 +0900

    update Panel

---
+++ Assets/02.Scripts/PanelBtn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelBtn : MonoBehaviour
+{
+    public GameObject PanelAlpha;
+
+    CanvasGroup OpenCanvasGroup;
+    CanvasGroup CloseCanvasGroup;
+
+
+    public void OnBtnClick()
+    {
+        OpenCanvasGroup = PanelAlpha.GetComponent<CanvasGroup>();
+        OpenCanvasGroup.alpha = 1;
+        OpenCanvasGroup.blocksRaycasts = true;
+    }
+
+    public void OffBtnClick()
+    {
+        CloseCanvasGroup = PanelAlpha.GetComponent<CanvasGroup>();
+        CloseCanvasGroup.alpha = 0;
+        CloseCanvasGroup.blocksRaycasts = false;
+    }
+}

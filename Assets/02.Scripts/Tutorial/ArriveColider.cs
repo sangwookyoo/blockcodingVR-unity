8474f8d9916f0ebfa236f01b5e6c7573c8e9db98
유상욱 <tkddnr032@gmail.com>
2021-06-17 16:12:38 +0900

    튜토리얼 렌더 텍스처

---
+++ Assets/02.Scripts/Tutorial/ArriveColider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ArriveColider : MonoBehaviour
+{
+    public string sceneName;
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.name == "Player")
+        {
+            SceneManager.LoadSceneAsync(sceneName);
+            Debug.Log(sceneName + "로 이동합니다.");
+        }
+    }
+}

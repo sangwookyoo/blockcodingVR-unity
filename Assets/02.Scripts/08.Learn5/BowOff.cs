c5f1e9997546e860e9291a4ca162e4479b51cc52
한솔 <80498475+amozo00@users.noreply.github.com>
2021-08-03 12:18:22 +0900

    씬 추가 및 변경
    1. 맵 - 콜라이더 수정 ㄱㄴㄷㄹPT
    2. T맵에 화살 스크립트 추가
    3. 학습모드 메뉴 추가
    
    게이미피케이션 vr 전환은 아직 수정중

---
+++ Assets/02.Scripts/08.Learn5/BowOff.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowOff : MonoBehaviour
+{
+    //솔, 충돌 시 화살 오브젝트 비활성화
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.name == "Player")
+        {
+            Debug.Log("충돌감지");
+            this.gameObject.SetActive(false);
+        }
+    }
+}

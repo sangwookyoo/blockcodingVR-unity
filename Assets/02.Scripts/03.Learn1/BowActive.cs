73fcc4c7f34d148164df10f38954655b425e6d37
한솔 <80498475+amozo00@users.noreply.github.com>
2021-08-18 20:26:14 +0900

    학습모드 오류수정
    - 카메라 화면 전환 오류수정
    - 위치 수정
    - vr카메라 전환 시 raycast선 삭제

--- Assets/02.Scripts/03.Learn1/BowActive.cs
+++ Assets/02.Scripts/03.Learn1/BowActive.cs
@@ -9,13 +9,25 @@ public class BowActive : MonoBehaviour
     public GameObject Bow;
     public GameObject Monster;
 
+    //솔, 카메라 시점 함수 추가
+    public GameObject Camera_1;
+    Camera cam1;
+    public GameObject Camera_3;
+    Camera cam3;
+
     void OnTriggerEnter(Collider c)
     {
         if (c.name == "Player")
         {
             Debug.Log("충돌감지");
             VivePlayer.SetActive(false);
+            cam1 = Camera_1.GetComponent<Camera>();
+            cam1.enabled = false;
+
             SteamVRPlayer.SetActive(true);
+            cam3 = Camera_1.GetComponent<Camera>();
+            cam3.enabled = true;
+
             Bow.SetActive(true);
             Monster.SetActive(true);
         }

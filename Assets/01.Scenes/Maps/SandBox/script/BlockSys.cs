a0f778d9707aa9aed602ed226d628d869c91a5b8
wlsdl8899 <78059088+wlsdl8899@users.noreply.github.com>
2021-08-30 15:52:00 +0900

    샌드박스

---
+++ Assets/01.Scenes/Maps/SandBox/script/BlockSys.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+// 블록 색 변경 시스템 스크립트
+
+public class BlockSys : MonoBehaviour
+{
+
+    public GameObject[] spriteArray;
+
+
+
+    [SerializeField]
+    private BlockType[] allBlockTypes;
+
+
+
+    [HideInInspector]
+    public Dictionary<int, BlockS> allBlocks = new Dictionary<int, BlockS>();
+
+
+    private int spriteArrayIndex;
+    int count = 1;
+    int count1 = 0;
+
+    public void Start()
+    {
+        spriteArrayIndex = 0;
+        spriteArray[0].SetActive(true);
+
+
+    }
+
+    public void Awake()
+    {
+        for (int i = 0; i < allBlockTypes.Length; i++)
+        {
+            BlockType newBlockType = allBlockTypes[i];
+            BlockS newBlock = new BlockS(i, newBlockType.blockName, newBlockType.blockMat);
+            allBlocks[i] = newBlock;
+            UnityEngine.Debug.Log("Block added to dictionary " + allBlocks[i].blockName);
+
+        }
+
+    }
+
+
+    public void ScreenTapUp()
+    {
+
+        if (count < spriteArray.Length)
+        {
+            spriteArray[count].SetActive(true);
+        }
+        count++;
+
+        if (count >= spriteArray.Length)
+
+            count = 0;
+
+
+
+
+    }
+
+    public void ScreenTapDown()
+    {
+        if (count1 < spriteArray.Length)
+        {
+            spriteArray[count1].SetActive(false);
+
+        }
+        count1++;
+
+        if (count1 >= spriteArray.Length)
+
+            count1 = 0;
+    }
+}
+
+
+
+
+public class BlockS
+{
+
+    public int blockID;
+
+    public string blockName;
+    public Material blockMaterial;
+
+    public BlockS(int id, string name, Material mat)
+    {
+        blockID = id;
+        blockName = name;
+        blockMaterial = mat;
+
+    }
+}
+
+[Serializable]
+public struct BlockType
+{
+
+    public string blockName;
+    public Material blockMat;
+}
---
+++ Assets/01.Scenes/Maps/SandBox/script/SetActive1.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SetActive1 : MonoBehaviour
+{
+    public SteamVR_Input_Sources leftHand = SteamVR_Input_Sources.LeftHand;
+    public SteamVR_Input_Sources rightHand = SteamVR_Input_Sources.RightHand;
+    public SteamVR_Input_Sources any = SteamVR_Input_Sources.Any;
+    public SteamVR_Action_Boolean trigger = SteamVR_Actions.default_InteractUI;
+    public SteamVR_Action_Boolean Grip = SteamVR_Actions.default_GrabGrip;
+    public SteamVR_Action_Boolean Trackpad = SteamVR_Actions.default_Teleport;
+
+    private bool state;
+
+
+    public GameObject Canvas1;
+   
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Trackpad.GetStateDown(rightHand))
+        {
+            if (state == true)
+            {
+                Canvas1.gameObject.SetActive(false);
+                state = false;
+                Debug.Log("비활성화");
+
+            }
+            else
+            {
+                Canvas1.gameObject.SetActive(true);
+                state = true;
+                Debug.Log("활성화");
+
+
+            }
+        }
+    }
+}
